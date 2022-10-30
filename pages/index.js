f8bf23696ad77ddc4a2ea8986748cc2e875075fb
Mitul Shah <mitulxshah@gmail.com>
2022-10-29 20:43:15 -0400

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,85 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "@/components/Link";
+import * as Accordion from "@radix-ui/react-accordion";
+import * as Collapsible from "@radix-ui/react-collapsible";
+import * as React from "react";
 
 export default function Home() {
+  const TableRow = () => {
+    const [open, setOpen] = React.useState(false);
+
+    return (
+      <Collapsible.Root asChild>
+        <li>
+          <Collapsible.Trigger className="w-full grid grid-cols-4 text-left py-2">
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+          </Collapsible.Trigger>
+          <Collapsible.Content>Yo</Collapsible.Content>
+        </li>
+      </Collapsible.Root>
+    );
+  };
+
+  //   const TableRow = () => {
+  //     return (
+  //       <Accordion.Item>
+  //         <Accordion.Trigger asChild>
+  //           <tr>
+  //             <td>Prospect Park</td>
+  //             <td>Prospect Park West and 9th St.</td>
+  //             <td>6am - 1am</td>
+  //             <td>
+  //               <a href="https://www.prospectpark.org/">Visit</a>
+  //             </td>
+  //           </tr>
+  //         </Accordion.Trigger>
+  //         <Accordion.Content>Content</Accordion.Content>
+  //       </Accordion.Item>
+  //     );
+  //   };
+
   return (
     <div className="">
       <h1>Places to Read</h1>
       <Link href="/about">About</Link>
-      <table>
-        <caption className="sr-only">Table caption</caption>
-        <thead>
-          <tr role="row">
-            <th>Park</th>
-            <th>Address</th>
-            <th>Hours</th>
-            <th>Website</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr role="row">
-            <td>Central Park</td>
-            <td>59th St. and 5th Ave.</td>
-            <td>6am - 1am</td>
-            <td>
-              <a href="https://www.centralparknyc.org/">Visit</a>
-            </td>
-          </tr>
-          <tr role="row">
-            <td>Prospect Park</td>
-            <td>Prospect Park West and 9th St.</td>
-            <td>6am - 1am</td>
-            <td>
-              <a href="https://www.prospectpark.org/">Visit</a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="w-full">
+        {/* <caption className="sr-only">Table caption</caption> */}
+        <div className="text-left border-b-2 border-supreme text-xl">
+          <ul className="w-full grid grid-cols-4">
+            <li>Park</li>
+            <li>Address</li>
+            <li>Hours</li>
+            <li>Website</li>
+          </ul>
+        </div>
+
+        <ul className="divide-y border-b divide-black border-black">
+          {/* <tr role="row">
+              <td>Central Park</td>
+              <td>59th St. and 5th Ave.</td>
+              <td>6am - 1am</td>
+              <td>
+                <a href="https://www.centralparknyc.org/">Visit</a>
+              </td>
+            </tr>
+            <tr role="row">
+              <td>Prospect Park</td>
+              <td>Prospect Park West and 9th St.</td>
+              <td>6am - 1am</td>
+              <td>
+                <a href="https://www.prospectpark.org/">Visit</a>
+              </td>
+            </tr> */}
+          <TableRow />
+          <TableRow />
+          <TableRow />
+          <TableRow />
+        </ul>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        supreme: "#000",
+      },
+    },
   },
   plugins: [],
 };
