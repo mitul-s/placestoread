ba90f36c6ab930eb8b4b479edb633d7bbd33b5e0
Mitul Shah <mitulxshah@gmail.com>
2022-11-01 00:19:58 -0400

    footer always at bottom

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Link from "@/components/Link";
 
 export default function Footer() {
   return (
-    <footer className="w-full px-4 py-2 text-white bg-mcqueen">
+    <footer className="w-full px-4 py-2 mt-auto text-white bg-mcqueen">
       <Link
         href="https://twitter.com/typicalmitul"
         isExternal
