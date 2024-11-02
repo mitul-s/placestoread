303153ae8dfa44adce3d5a50dc4df8a9a0ca38be
mitul-s <mitulxshah@gmail.com>
2024-11-01 22:23:54 -0400

    vamoss

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Link from "@/components/Link";
 import Arrow from "@/components/Arrow";
+import MagnifyingGlass from "@/components/MagnifyingGlass";
 import * as Collapsible from "@radix-ui/react-collapsible";
 import { google } from "googleapis";
 import React from "react";
@@ -217,14 +218,17 @@ export default function Home({ parks }) {
 
   return (
     <main className="w-full h-full">
-      <div className="relative">
-        <input
-          type="text"
-          placeholder="Search parks..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="border-y border-mcqueen w-full px-4 py-2.5 mb-4 outline-none placeholder:text-mcqueen/50 font-medium bg-mcqueen/5 border-b-0"
-        />
+      <div className="relative border-t sm:border border-mcqueen w-full font-medium bg-mcqueen/20 focus-within:bg-mcqueen/5 transition-all duration-200 grid grid-cols-yeah sm:mb-12 sm:-mt-4">
+        <div className="col-[1/-1]">
+          <MagnifyingGlass className="w-5 h-5 text-mcqueen/60 absolute top-1/2 left-4 -translate-y-1/2 pointer-events-none" />
+          <input
+            type="text"
+            placeholder="Search for parks..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-full h-full outline-none placeholder:text-mcqueen/60 bg-transparent pr-4 pl-16 sm:pl-12 py-4 sm:py-2.5"
+          />
+        </div>
       </div>
       <ul className="sm:hidden grid w-full divide-y divide-mcqueen border-t-2 border-mcqueen">
         {filteredParks.map((park) => (
